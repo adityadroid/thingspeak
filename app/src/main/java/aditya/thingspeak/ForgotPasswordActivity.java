08c776739cde3b8d49f2cc2edc28b9c2ae4eb881
eminemoholic <adityaadi1467@gmail.com>
2017-02-23 20:54:54 +0530

    Forgot password,user login completed

---
+++ app/src/main/java/aditya/thingspeak/ForgotPasswordActivity.java
@@ -0,0 +1,66 @@
+package aditya.thingspeak;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Explode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    Button btResetPwd;
+    FirebaseAuth auth;
+    String emailAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        Explode explode = new Explode();
+        explode.setDuration(500);
+        getWindow().setExitTransition(explode);
+        getWindow().setEnterTransition(explode);
+        auth = FirebaseAuth.getInstance();
+
+        etEmail=(EditText)findViewById(R.id.et_email);
+        btResetPwd= (Button)findViewById(R.id.bt_reset_pwd);
+        btResetPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.progressIndicator).setVisibility(View.VISIBLE);
+
+                if(etEmail.getText().toString().isEmpty()){
+                    Snackbar.make(btResetPwd,"One or more fields empty!",Snackbar.LENGTH_SHORT).show();
+
+                }else{
+                    emailAddress=etEmail.getText().toString();
+                      auth.sendPasswordResetEmail(emailAddress)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    findViewById(R.id.progressIndicator).setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        Log.d("SENT", "Email sent.");
+                                        Snackbar.make(btResetPwd,"A reset link has been sent to your email.",Snackbar.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        Snackbar.make(btResetPwd,"Try again!",Snackbar.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/aditya/thingspeak/MainActivity.java
+++ app/src/main/java/aditya/thingspeak/MainActivity.java
@@ -4,24 +4,39 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.transition.Explode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-    EditText etPassword;
-    Button btGo;
-    EditText etUsername;
+    private FirebaseAuth mAuth;
+    private EditText etPassword;
+    private Button btGo;
+    private EditText etUsername;
     CardView cv;
     FloatingActionButton fab;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    TextView forgotPasswordTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
         etUsername= (EditText)findViewById(R.id.et_username);
         cv = (CardView)findViewById(R.id.cv);
         fab = (FloatingActionButton)findViewById(R.id.fab);
-        btGo.setOnClickListener(new View.OnClickListener() {
+        forgotPasswordTextView= (TextView)findViewById(R.id.forgot_password_textview);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Explode explode = new Explode();
@@ -41,10 +60,22 @@ public class MainActivity extends AppCompatActivity {
                 getWindow().setExitTransition(explode);
                 getWindow().setEnterTransition(explode);
                 ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this);
-                Intent i2 = new Intent(MainActivity.this,HomeActivity.class);
+                Intent i2 = new Intent(MainActivity.this,ForgotPasswordActivity.class);
                 startActivity(i2, oc2.toBundle());
+
             }
         });
+
+        btGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    if(validateData()) {
+                        signInUser(etUsername.getText().toString().trim(),etPassword.getText().toString().trim());
+                    }
+            }
+        });
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,6 +91,48 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
+    }
+    private boolean validateData(){
+        if(etUsername.getText().toString().trim().isEmpty()||etPassword.getText().toString().isEmpty()){
+            Snackbar.make(btGo,"One or more fields empty",Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void signInUser(String email,String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                            Explode explode = new Explode();
+                            explode.setDuration(500);
+
+                            getWindow().setExitTransition(explode);
+                            getWindow().setEnterTransition(explode);
+                            ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this);
+                            Intent i2 = new Intent(MainActivity.this,HomeActivity.class);
+                            startActivity(i2, oc2.toBundle());
+                        }else{
+                            Log.d("Failed", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(MainActivity.this,"Failed!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 
 
